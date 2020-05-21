ffddd98721ebaecee099ace4684a28b39d8ce105
Duur Klop <duurklop@gmail.com>
2020-05-22 00:19:32 +0200

    created test for deleting model

--- plugins/essif-lab/tests/Stubs/Utility.php
+++ plugins/essif-lab/tests/Stubs/Utility.php
@@ -9,6 +9,8 @@ use TNO\EssifLab\Utilities\WP;
 class Utility extends BaseUtility {
 	private $history = [];
 
+	static $meta = array();
+
 	protected $callbackTriggeringFunctions = [
 		WP::ADD_ACTION => [self::class, 'addHook'],
 		WP::ADD_FILTER => [self::class, 'addHook'],
@@ -18,11 +20,13 @@ class Utility extends BaseUtility {
 	protected $valueReturningFunctions = [
 		BaseUtility::GET_CURRENT_MODEL => [self::class, 'getCurrentModel'],
         BaseUtility::GET_MODEL => [self::class, 'getModel'],
+        BaseUtility::CREATE_MODEL_META => [self::class, 'createModelMeta'],
+        BaseUtility::DELETE_MODEL_META => [self::class, 'deleteModelMeta'],
 		BaseUtility::GET_MODEL_META => [self::class, 'getModelMeta'],
 		BaseUtility::GET_MODELS => [self::class, 'getModels'],
 	];
 
-	function call(string $name, ...$parameters) {
+    function call(string $name, ...$parameters) {
 		$wasCalled = count($this->getHistoryByFuncName($name));
 		$this->history[] = new History($name, $parameters, $wasCalled + 1);
 
@@ -60,32 +64,66 @@ class Utility extends BaseUtility {
 	}
 
     static function getModel(int $id): Model {
-        return new Model([
-            Constants::TYPE_INSTANCE_IDENTIFIER_ATTR => $id,
-            Constants::TYPE_INSTANCE_TITLE_ATTR => 'hello',
-            Constants::TYPE_INSTANCE_DESCRIPTION_ATTR => 'world',
-        ]);
+        return self::createModelWithId($id);
     }
 
 	static function getCurrentModel(): Model {
-		return new Model([
-			Constants::TYPE_INSTANCE_IDENTIFIER_ATTR => 1,
-			Constants::TYPE_INSTANCE_TITLE_ATTR => 'hello',
-			Constants::TYPE_INSTANCE_DESCRIPTION_ATTR => 'world',
-		]);
+		return self::createModelWithId(1);
 	}
 
-	static function getModelMeta(): array {
+	static function createModelMeta(int $postId, string $key, $value): bool {
+	    if (!isset(self::$meta[$postId])){
+	        self::$meta[$postId] = array();
+        }
+	    if (!isset(self::$meta[$postId][$key])){
+	        self::$meta[$postId][$key] = array();
+        }
+	    self::$meta[$postId][$key][] = $value;
+	    return true;
+    }
+
+    static function deleteModelMeta(int $postId, string $key, $value = ''): bool {
+        if (self::checkPostIdAndKey($postId, $key)){
+            if (!empty($value)){
+                if (($value_key = array_search($value, self::$meta[$postId][$key])) !== false){
+                    unset(self::$meta[$postId][$key][$value_key]);
+                    return !in_array($value, self::$meta[$postId][$key]);
+                }
+            } else {
+                unset(self::$meta[$postId][$key]);
+                return !self::checkPostIdAndKey($postId, $key) ? true : false;
+            }
+        }
+        return false;
+    }
+
+	static function getModelMeta(int $postId, string $key): array {
+	    if (isset(self::$meta) && isset(self::$meta[$postId]) && isset(self::$meta[$postId][$key])){
+            return self::$meta[$postId][$key];
+        }
 		return [1];
 	}
 
-	static function getModels(): array {
-		return [
-			new Model([
-				Constants::TYPE_INSTANCE_IDENTIFIER_ATTR => 1,
-				Constants::TYPE_INSTANCE_TITLE_ATTR => 'hello',
-				Constants::TYPE_INSTANCE_DESCRIPTION_ATTR => 'world',
-			]),
-		];
-	}
+	static function getModels(array $args = []): array {
+	    if (!empty($args) && !empty($args['post__in'])){
+            return array_map(function($id) {
+                return self::createModelWithId($id);
+            }, $args['post__in']);
+        }
+        return [self::createModelWithId(1)];
+    }
+
+    static function createModelWithId($id): Model
+    {
+        return new Model([
+            Constants::TYPE_INSTANCE_IDENTIFIER_ATTR => $id,
+            Constants::TYPE_INSTANCE_TITLE_ATTR => 'hello',
+            Constants::TYPE_INSTANCE_DESCRIPTION_ATTR => 'world'
+        ]);
+    }
+
+    private static function checkPostIdAndKey(int $postId, string $key): bool
+    {
+        return isset(self::$meta) && isset(self::$meta[$postId]) && isset(self::$meta[$postId][$key]);
+    }
 }
